8ab6e8dd5ddf40f9457ff4263ecc104f07acbcc2
michael clark <michaelclark@michaels-Air.fios-router.home>
2021-02-08 10:22:33 -0500

    my first commit

---
+++ app.js
@@ -0,0 +1,193 @@
+console.log('yyyuurrr')
+
+// //////    OBJECTS    \\\\\\\\\\
+const catQuestions =  {
+    science: {
+        100: { 
+            Question: 'Molecular structure of water',
+            Answers: ['h2o2', 'h20', 'ho2']
+        },
+        200: {
+            Question: 'Your body produces this in high quantity when sick',
+            Answers: [ 'Red blood cells','White blood cells', 'Platelets'],
+        },
+        300: {
+            Question: 'This kind of acid builds up in your body when exercising',
+            Answers: ['Sulfuric acid', 'phospherous acid,', 'Lactic acid'],
+        },
+        400: {
+            Question: 'example question',
+            Answers: ['answer1', 'answer2', 'answer3'],
+        },
+        500: {
+            Question: '500 example question',
+            Answers: ['answer1', 'answer2','answer3'],
+    },
+    Math: {
+        100: {
+            Question: 'You have 2 apples I take away 1 orange.  How many apples do you have left?',
+            Answers: ['3', '1', '2'],
+        },
+        200: {
+            Question: 'which of the following is a prime number?',
+            Answers: ['9', '22', '11'],
+        },
+        300: {
+            Question: 'when operating with numbers which should you do first',
+            Answers: ['exponents', 'multiplication', 'division'],
+        },
+        400: {
+            Question: ' ',
+            Answers: [' ' ],
+        },
+        500: {
+            Question: 'What\'s 9 + 10?',
+            Answers: ['21', '9', '19'],
+    },
+    Anime: {
+        100: { 
+            Question: 'In this anime there are 7 golden spheres to collect',
+            Answers: ['Ball Collecting Adventure 2', 'Dragonball', 'Dragon Tails'],
+        },
+        200: {
+            Question:'This character from naruto is most famous for his signature Rasengan',
+            Answers: ['Chidori', 'Sasuke', 'Naruto'],
+        },
+        300: {
+            Question: ' ', 
+            Answers: [' '],
+        },
+        400: {
+            Question: ' ',
+            Answers: [' ']
+        },
+        500:{
+            Question: 'This anime is commonly known as the greatest anime of all time',
+            Answers: ['Steins;Gate', 'Steins; Gate', 'Steins; Gate'],
+        },
+    videoGames: {
+        100: {
+            Question: 'A popular battle royale featuring pinatas and building',
+            Answers: ['Fork Knife', 'Viva Pinata', 'Fortnite'],
+        },
+        200: {
+            Question: 'Iconic series feturing Master Chief',
+            Answers: ['Cooking Mama', 'Pokemon', 'Halo'],
+        },
+        300: {
+            Question: 'This franchise first released in 1986 and features a male hero from the Hylian race. The legend of :',
+            Answers: ['Mario', 'Zelda', 'Link'],
+        },
+        400: {
+            Question: '  ',
+            Answers:[' '],
+        },
+        500: {
+            Question: ' ',
+            Answers: [' '],
+        },
+    },
+},
+    
+
+   
+        
+
+// const createBoard = () => {
+//     for (let i = 0; i < 5; i++) {
+//         for (let j = 0; j < 5; j++) {
+//             if (i = 0) {
+//                 $('<div>').attr('id', 'CAT1' + j).appendTo('body')
+//             } else if (i = 1) {
+//                 $('<div>').attr('id','CAT2'+ j).appendTo('body')
+//             } else if (i = 2) {
+//                 $('<div>').attr('id', 'CAT3' + j).appendTo('body')
+//             } else if (i = 3) {
+//                 $('<div>').attr('id', 'CAT4' + j).appendTo('body')
+//             } else if (i = 4) {
+//                 $('<div>').attr('id', 'CAT5' + j).appendTo('body')
+//             } 
+//         } 
+//     }
+// }
+
+
+
+
+
+const onChoiceClick  = (event) => {
+    console.log($(event.currentTarget))
+    $(event.currentTarget).addClass('selected')
+    answering()
+}
+
+
+const answering = (currentPlayer) => {
+    if (correctAnswer = true) {
+        alert('You were correct.  Chose another question.')
+        // currentPlayer score ++
+    } else {
+        alert('That was incorrect! Next players turn.') 
+        // switch player
+    }
+}
+ 
+
+
+
+
+
+$(() => {
+    createBoard()
+    // $('table').on('click', eventTest)
+    $('td').on('click', onChoiceClick) 
+
+})
+
+
+
+const createBoard = () => {
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+            switch(i) {
+            case 0: 
+                $('<div>').attr('id', 'cat' + j).appendTo('#container')
+                break;
+            case 1:
+                $('<div>').attr('id', 'science' + j).appendTo('#container')
+                break;
+            case 2:
+                $('<div>').attr('id', 'CAT3' + j).appendTo('#container')
+                break;
+            case 3:
+                $('<div>').attr('id', 'CAT4' + j).appendTo('#container')
+                break;
+            case 4:
+                $('<div>').attr('id', 'CAT5' + j).appendTo('#container')
+                        
+            }    
+        } 
+    }
+}
+
+
+let player1;
+let player2;
+
+// const onChoiceClick  = (event) => {
+//     console.log($(event.currentTarget))
+//     $(event.currentTarget).addClass('selected')
+//     answering()
+// }
+
+
+// const answering = (currentPlayer) => {
+//     if (correctAnswer = true) {
+//         alert('You were correct.  Chose another question.')
+//         currentPlayer += pointValue 
+//     } else {
+//         alert('That was incorrect! Next players turn.') 
+//         // switch player
+//     }
+// }
+ 
