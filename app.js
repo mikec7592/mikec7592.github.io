a8fef621819ab1ffd2d1399b0fe8197e34e1e5c1
michael clark <michaelclark@michaels-MacBook-Air.local>
2021-02-08 21:08:14 -0500

    object formatted, correct values being accessed when toggling submit button

--- app.js
+++ app.js
@@ -46,13 +46,12 @@ const onChoiceClick  = (event) => {
     console.log(catQuestions)
     $(event.currentTarget).addClass('selected').text(catQuestions[name][value].Question);
     // on click radio buttons with answers are sent to a div below the table
-                console.log(catQuestions[name][value].Answers.length)
         // $form.appendTo('body')
       for  (let i = 0; i < Object.keys(catQuestions[name][value].Answers).length; i++) {
-          console.log(Object.keys(catQuestions[name][value].Answers)[i])
-        $('<input type ="radio" name="Answers" value="catQuestions[name][value].Answers[i]">').appendTo('form')
-        $('<label for="catQuestions[name][value].Answers[i]">').text(Object.keys(catQuestions[name][value].Answers)[i]).appendTo('form')
-
+        let answer = Object.keys(catQuestions[name][value].Answers)[i];
+        let answerValue = Object.values(catQuestions[name][value].Answers)[i].value;
+        $('<input type ="radio" name="Answers" value="'+ answerValue +'">').appendTo('form')
+        $('<label for="' + answer +'">').text(Object.keys(catQuestions[name][value].Answers)[i]).appendTo('form')
     }
     $('<input type="submit" value="Submit">').appendTo('form')
     // answering()
@@ -61,6 +60,8 @@ const onChoiceClick  = (event) => {
 const onSubmit = (event) => {
     console.log()
     event.preventDefault()
+    let $selection = $('input[name = Answers]:checked', 'form').val()
+    console.log($selection)
     $('form').children().remove()
 }
 
@@ -144,94 +145,220 @@ const catQuestions = {
     'Math': {
         100: {
             Question: 'You have 2 apples I take away 1 orange. How many apples do you have left?',
-            Answers: ['3', '1', '2'],
+            Answers: {
+                '3': {
+                    value: false
+                }, 
+                '1': {
+                    value: false
+                }, 
+                '2': {
+                    value: true
+                },
         },
+    },
         200: {
             Question: 'which of the following is a prime number?',
-            Answers: ['9', '22', '11'],
+            Answers: {
+                '9': {
+                    value: false
+                }, 
+                '22': {
+                    value: false
+                }
+                , '11': {
+                    value: true
+                },
         },
+    },
         300: {
             Question: 'when operating with numbers which should you do first',
-            Answers: ['exponents', 'multiplication', 'division'],
+            Answers: {
+                'exponents': {
+                    value: true
+                }, 'multiplication': {
+                    value: false
+                }, 'division': {
+                    value: false
+                },
         },
+    },
         400: {
             Question: ' ',
-            Answers: [' '],
+            Answers: {
+                'apple': {
+                    value: false
+                },
+                'mango': {
+                    value: true
+                },
+                'potato': {
+                    value: false
+                }
         },
+    },
         500: {
             Question: 'What\'s 9 + 10?',
-            Answers: ['21', '9', '19'],
+            Answers: {
+                '21': {
+                    value: true
+                }, 
+                '9': {
+                    value: false
+                }, 
+                '19': {
+                    value: false
+                },
         }
     },
+},
     'Anime': {
         100: {
             Question: 'In this anime there are 7 golden spheres to collect',
-            Answers: ['Ball Collecting Adventure 2', 'Dragonball', 'Dragon Tails'],
+            Answers: {
+                'Ball Collecting Adventure 2': {
+                    value: false
+                }, 
+                'Dragonball': {
+                    value: true
+                }, 
+                'Dragon Tails': {
+                    value: false
+                },
         },
+    },
         200: {
             Question: 'This character from naruto is most famous for his signature Rasengan',
-            Answers: ['Chidori', 'Sasuke', 'Naruto'],
+            Answers: {
+                'Chidori': {
+                    value: false
+                }, 
+                'Sasuke': {
+                    value: false
+                }, 
+                'Naruto': {
+                    value: true
+                },
         },
+    },
         300: {
             Question: ' ',
-            Answers: [' '],
+            Answers: {
+                'your': {
+                    value: false
+                },
+                'hello': {
+                    value: true
+                },
+                'toes': {
+                    value: false
+                }
         },
+    },
         400: {
             Question: ' ',
-            Answers: [' '],
+            Answers: {
+                 'son ': {
+                     value: true
+                 },
+                 'kai ': {
+                     value: false
+                 },
+                 'yyuurr': {
+                     value: false
+                 }
         },
+    },
         500: {
             Question: 'This anime is commonly known as the greatest anime of all time',
-            Answers: ['Steins;Gate', 'Steins; Gate', 'Steins; Gate'],
+            Answers: {
+                'Steins;Gate': {
+                    value: true
+                }, 
+                'Steins; Gate': {
+                    value: true
+                }, 
+                'Steins; Gate': {
+                    value: true
+                },
         }
     },
+},
     'videoGames': {
         100: {
             Question: 'A popular battle royale featuring pinatas and building',
-            Answers: ['Fork Knife', 'Viva Pinata', 'Fortnite'],
+            Answers: {
+                'Fork Knife': {
+                    value: false
+                }, 
+                'Viva Pinata': {
+                    value: false
+                }, 
+                'Fortnite': {
+                    value: true
+                },
         },
+    },
         200: {
             Question: 'Iconic series feturing Master Chief',
-            Answers: ['Cooking Mama', 'Pokemon', 'Halo'],
+            Answers: {
+                'Cooking Mama': {
+                    value: false
+                }, 
+                'Pokemon': {
+                    value: false
+                }, 
+                'Halo': {
+                    value: true
+                },
         },
+    },
         300: {
             Question: 'This franchise first released in 1986 and features a male hero from the Hylian race. The legend of :',
-            Answers: ['Mario', 'Zelda', 'Link'],
+            Answers: {
+                'Mario': {
+                    value: false
+                }, 
+                'Zelda': {
+                    value: true
+                }, 
+                'Link': {
+                    value: false
+                },
         },
+    },
         400: {
             Question: ' ',
-            Answers: [' '],
+            Answers: {
+                'tinos': {
+                    value: false
+                },
+                'binos': {
+                    value: false
+                },
+                'minos': {
+                    value: true
+                },
         },
+    },
         500: {
             Question: ' ',
-            Answers: [' ']
+            Answers: {
+                'ggrrr': {
+                    value: false
+                },
+                'yyuurrr': {
+                    value: true
+                },
+                'skurskur': {
+                    value: false
+                }
         }
     }
 }
+}
     
     
-// console.log(catQuestions.science)
-
-   
-        
-
-// const createBoard = () => {
-//     for (let i = 0; i < 5; i++) {
-//         for (let j = 0; j < 5; j++) {
-//             if (i = 0) {
-//                 $('<div>').attr('id', 'CAT1' + j).appendTo('body')
-//             } else if (i = 1) {
-//                 $('<div>').attr('id','CAT2'+ j).appendTo('body')
-//             } else if (i = 2) {
-//                 $('<div>').attr('id', 'CAT3' + j).appendTo('body')
-//             } else if (i = 3) {
-//                 $('<div>').attr('id', 'CAT4' + j).appendTo('body')
-//             } else if (i = 4) {
-//                 $('<div>').attr('id', 'CAT5' + j).appendTo('body')
-//             } 
-//         } 
-//     }
-// }
 
 
 
@@ -255,67 +382,3 @@ const catQuestions = {
 
 
 
-
-
-// const createBoard = () => {
-//     let amount = 100;
-
-//     $('<table><thead><tbody><tr>').appendTo('body')
-
-//     for (let i = 0; i < 4; i++){
-//         $('<tr>').attr('id', amount).appendTo('tbody')
-//         $('<th>').addClass('box').text(Object.keys(catQuestions)[i]).appendTo('thead')
-//         for (let j = 0; j < 4; j++) {
-//             $('<td>').addClass('box').attr('id', Object.keys(catQuestions)[j]+ ' ' + amount).text(amount).appendTo('#' + amount)
-//         } 
-//         amount += 100
-//     }
-// }
-
-
-
-// const createBoard = () => {
-//     for (let i = 0; i < 6; i++) {
-//         for (let j = 0; j < 6; j++) {
-//             switch(i) {
-//             case 0: 
-//                 $('<div>').attr('id', 'cat' + j).appendTo('#container')
-//                 break;
-//             case 1:
-//                 $('<div>').attr('id', 'science' + j).appendTo('#container')
-//                 break;
-//             case 2:
-//                 $('<div>').attr('id', 'CAT3' + j).appendTo('#container')
-//                 break;
-//             case 3:
-//                 $('<div>').attr('id', 'CAT4' + j).appendTo('#container')
-//                 break;
-//             case 4:
-//                 $('<div>').attr('id', 'CAT5' + j).appendTo('#container')
-                        
-//             }    
-//         } 
-//     }
-// }
-
-
-// let player1;
-// let player2;
-
-// const onChoiceClick  = (event) => {
-//     console.log($(event.currentTarget))
-//     $(event.currentTarget).addClass('selected')
-//     answering()
-// }
-
-
-// const answering = (currentPlayer) => {
-//     if (correctAnswer = true) {
-//         alert('You were correct.  Chose another question.')
-//         currentPlayer += pointValue 
-//     } else {
-//         alert('That was incorrect! Next players turn.') 
-//         // switch player
-//     }
-// }
-// 
